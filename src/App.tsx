f58624e4056cd5fb5eb1a13254f0a88113c1c9bf
makshakov <888makshakov888@gmail.com>
2023-04-03 14:00:46 +0300

    pilot version

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,57 @@
 import React, {useEffect, useState} from 'react'
 import './App.scss'
 
+type objType = {
+    id: number,
+    text: string,
+    discrption: string,
+}
 const App = () => {
 
     useEffect(() => {
-        const fromLS = localStorage.getItem("test")
-        console.log(fromLS, 'fromLS')
+        const fromLS: string | null = localStorage.getItem("test")
         if (fromLS) {
-            const newMass = fromLS && JSON.parse(fromLS)
-            const sortMass = newMass.sort((a: any, b: any) => a.text.toLowerCase() > b.text.toLowerCase() ? 1 : -1)
+            const newMass = JSON.parse(fromLS)
+            const sortMass: objType[] = newMass.sort((a: objType, b: objType) => a.text.toLowerCase() > b.text.toLowerCase() ? 1 : -1)
             setMass(sortMass)
         }
     }, [])
 
     const [id, setId] = useState(0)
+
     const [editInput, setEditInput] = useState(false)
+    const [confirm, setConfirm] = useState(false)
+    const [showDaneModal, setShowDaneModal] = useState(false)
     const [editTextarea, setEditTextarea] = useState(false)
+    const [showMass, setShowMass] = useState(false)
+
     const [inputValue, setInputValue] = useState('')
     const [textareaValue, setTextareaValue] = useState('')
-    const [mass, setMass] = useState([
+
+    const [mass, setMass] = useState<objType[]>([
         {id: 1, text: 'в', discrption: 'discrption'}
     ])
 
+    useEffect(() => {
+        if (showDaneModal) {
+            setTimeout(() => {
+                setShowDaneModal(false)
+            }, 3000)
+        }
+    }, [showDaneModal])
+
+    if (confirm) {
+        document.body.style.overflow = "hidden"
+    } else {
+        document.body.style.overflow = "auto"
+    }
+
     const onTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        e.currentTarget.value && setTextareaValue(e.currentTarget.value)
+        setTextareaValue(e.currentTarget.value)
     }
 
     const onInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        e.currentTarget.value && setInputValue(e.currentTarget.value)
+        setInputValue(e.currentTarget.value)
 
     }
 
@@ -45,8 +69,8 @@ const App = () => {
         setMass(newMass)
         setInputValue('')
         setTextareaValue('')
-        localStorage.setItem("test", JSON.stringify(newMass))
     }
+
 
     const OneditInput = (e: React.MouseEvent<HTMLLIElement>, id: number, text: string) => {
         setEditTextarea(false)
@@ -58,23 +82,19 @@ const App = () => {
         setInputValue(e.currentTarget.value)
         const obj = mass.find(row => row.id === id)
         const lastMass = mass.filter(row => row.id !== id)
-        const neaMass = [...lastMass, {...obj, text: inputValue}]
-        // @ts-ignore
-        setMass(neaMass)
+        const neaMass = obj && lastMass && [...lastMass, {...obj, text: inputValue}]
+        neaMass && setMass(neaMass)
         !e.currentTarget.checked && setId(0)
         setInputValue('')
     }
 
-    const textAteaBlur = (e: React.FocusEvent<HTMLTextAreaElement>) => {
+    const textAreaBlur = (e: React.FocusEvent<HTMLTextAreaElement>) => {
         setTextareaValue(e.currentTarget.value)
         setTextareaValue('')
-
         const obj = mass.find(row => row.id === id)
         const lastMass = mass.filter(row => row.id !== id)
-        const neaMass = [...lastMass, {...obj, discrption: textareaValue}]
-        // @ts-ignore
-        setMass(neaMass)
-
+        const neaMass = obj && lastMass && [...lastMass, {...obj, discrption: textareaValue}]
+        neaMass && setMass(neaMass)
         setEditTextarea(false)
 
     }
@@ -86,16 +106,44 @@ const App = () => {
     }
 
     const deleteMass = (id: number) => {
-        const newMass = mass.filter(row => row.id !== id)
-        setMass(newMass)
-    }
+        if (confirm) {
+            const newMass = mass.filter(row => row.id !== id)
+            setMass(newMass)
+            setConfirm(false)
+        }
 
+        return
+    }
     return (
-        <div className='App'>
+        <div className={`App`}>
             <div className='header'>
-                <input className='input' value={!id ? inputValue : ''} onChange={(e) => onInput(e)} type="text"/>
+                <div className='inputAndSave'>
+                    <button className='button' onClick={() => {
+                        localStorage.setItem("test", JSON.stringify(mass))
+                        setShowDaneModal(true)
+                    }}>Сохранить перед выходом
+                    </button>
+                    <input className='input' value={inputValue} onChange={(e) => onInput(e)} type="text"/>
+                </div>
                 <textarea className='textarea' value={textareaValue} onChange={(e) => onTextarea(e)}/>
-                <button className='button' onClick={save}>save</button>
+
+                <div className='buttons'>
+                    <button className='button' onClick={save}>save</button>
+                    <button className='button' onClick={() => setShowMass(true)}>showMass</button>
+                </div>
+                {showMass && (
+                    <div className='showModal'>
+                        <span>{JSON.stringify(mass)}</span>
+                        <button onClick={() => setShowMass(false)} className='button'>Ok</button>
+                    </div>
+
+                )}
+                {showDaneModal && (
+                    <div className='showDaneModal'>
+                        <span>Успешно сохранено</span>
+                    </div>
+
+                )}
             </div>
             {
                 mass.map(row => <ul key={row.id} className='ul'>
@@ -116,12 +164,29 @@ const App = () => {
                                 className='textarea'
                                 value={textareaValue}
                                 onChange={(e) => onTextarea(e)}
-                                onBlur={(e) => textAteaBlur(e)}
+                                onBlur={(e) => textAreaBlur(e)}
                             />
                             : <li onClick={(e) => OneditTextarea(row.id, row.discrption)}>{row.discrption}</li>
                     }
-                    <button onClick={() => deleteMass(row.id)}>x</button>
+                    <li>
+                        <button
+                            onClick={() => {
+                                setId(row.id)
+                                setConfirm(true)
+                            }}
+                            className='button'>X
+                        </button>
+                    </li>
+                    {confirm && (
+                        <div className='showModalForDelete'>
+                            <span>Точно удалить <br/> "{row.text}" ?</span>
+                            <div className='buttons'>
+                                <button onClick={() => deleteMass(id)} className='button'>Ok</button>
+                                <button onClick={() => setConfirm(false)} className='button'>Отмена</button>
+                            </div>
+                        </div>
 
+                    )}
                 </ul>)
             }
         </div>
